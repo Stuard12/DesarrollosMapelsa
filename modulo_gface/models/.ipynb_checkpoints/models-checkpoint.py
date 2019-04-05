48c36c7cb20a5d56ee01bb0a74d61a3d9f117f6c
Stuard12 <48603168+Stuard12@users.noreply.github.com>
2019-04-05 15:59:32 +0000

    [Add] creacion de modulo1

---
+++ modulo_gface/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class configuracion(models.Model):
+    _name = 'modulo_gface.modulo_gface.configuracion'
+     
+    requestor = fields.Char(string='ID de cuenta', size=140, required=True)
+    transaction = fields.Char(string='Tipo de transaccion', size=140, required=True)
+    contry = fields.Char(string='Pais', size=140, required=True)
+    entity = fields.Char(string='Nit', size=140, required=True)
+    user = fields.Char(string='Usuario', size=140, required=True)
+    userName = fields.Char(string='Nombre de usuario', size=140, required=True)
+     
+# class modulo_gface(models.Model):
+#     _name = 'modulo_gface.modulo_gface'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
